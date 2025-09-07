d07d90ef17c167d199bb92f0368076985b5835ee
noboyboe <formaldavid7@gmail.com>
2025-09-06 23:58:26 -0400

    Add files via upload

---
+++ Assignment2.py
@@ -0,0 +1,42 @@
+# Program Name: Assignment1.py (use the name the program is saved as)
+# Course: IT3883/Section 01
+# Student Name: David Dinh
+# Assignment Number: Assignment #2
+# Due Date: 09/19/2025
+# Purpose:
+# List Specific resources used to complete the assignment.
+
+#Bob 100 34 25 22 76 87
+#Jack 43 54 99 63 101 44
+#Jane 78 98 45 74 65 23
+#Pete 99 65 101 56 33 47
+#Ann 78 21 22 101 44 100
+#Alice 100 102 89 99 92 85
+#John 45 66 99 99 89 78
+#Ben 45 85 99 99 89 88
+#Henry 45 12 78 98 65 32
+
+students = {
+    "bob" : [100,34,25,22,76,87],
+    "jack" : [43,54,99,63,101,44],
+    "jane" : [78,98,45,74,65,23],
+    "pete" : [99,65,101,56,33,47],
+    "ann" : [78,21,22,101,44,100],
+    "alice" : [100,102,89,99,92,85],
+    "john" : [45,66,99,99,89,78],
+    "ben" : [45,85,99,99,89,88],
+    "henry" : [45,12,78,98,65,32],
+}
+
+for i in students:
+    grade = 0
+
+
+    #grade = 0
+    #for x in i:
+       # grade += x
+    #average = grade / 6
+    #print(f"{students} has an average of {average}")
+
+
+
