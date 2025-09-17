c371fb9f139096dc9afdee6aee1ec6974ab40b68
noboyboe <formaldavid7@gmail.com>
2025-09-16 20:06:25 -0400

    Update Assignment2.py

--- Assignment2.py
+++ Assignment2.py
@@ -5,6 +5,10 @@
 # Due Date: 09/19/2025
 # Purpose:
 # List Specific resources used to complete the assignment.
+#https://www.w3schools.com/python/python_dictionaries.asp
+#https://www.w3schools.com/python/ref_dictionary_keys.asp
+#https://www.w3schools.com/python/ref_string_split.asp
+#https://www.geeksforgeeks.org/python/python-program-to-sort-the-list-according-to-the-column-using-lambda/
 
 #Bob 100 34 25 22 76 87
 #Jack 43 54 99 63 101 44
@@ -16,27 +20,28 @@
 #Ben 45 85 99 99 89 88
 #Henry 45 12 78 98 65 32
 
-students = {
-    "bob" : [100,34,25,22,76,87],
-    "jack" : [43,54,99,63,101,44],
-    "jane" : [78,98,45,74,65,23],
-    "pete" : [99,65,101,56,33,47],
-    "ann" : [78,21,22,101,44,100],
-    "alice" : [100,102,89,99,92,85],
-    "john" : [45,66,99,99,89,78],
-    "ben" : [45,85,99,99,89,88],
-    "henry" : [45,12,78,98,65,32],
-}
-
-for i in students:
-    grade = 0
+students = []
+grade_av = 0
+name = " "
 
 
-    #grade = 0
-    #for x in i:
-       # grade += x
-    #average = grade / 6
-    #print(f"{students} has an average of {average}")
+for x in range(9):
+    grade_total = 0
+
+    grades = input("").split()
+
+    input_name = grades[0]
+    input_grades = grades[1:]
+
+    for i in input_grades:
+        grade_total += int(i)
+    grade_av = grade_total / len(input_grades)
+    students.append((input_name, grade_av))
+
+students.sort(key=lambda x: x[1], reverse=True)
+
+for input_name, grade_av in students:
+    print(f"{input_name} {grade_av:.2f}")
 
 
 
