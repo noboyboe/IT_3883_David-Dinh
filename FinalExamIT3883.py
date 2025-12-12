b3fa6e12e3cf664eb41e8fcfc94915d88b2ddd0c
noboyboe <formaldavid7@gmail.com>
2025-12-11 19:27:56 -0500

    Update FinalExamIT3883.py

--- FinalExamIT3883.py
+++ FinalExamIT3883.py
@@ -1,5 +1,5 @@
 statement = input("State an amount of money: ")
-
+#Asks the user to input a statement
 def remove():
     item = statement.split()
     for x in item:
@@ -15,7 +15,7 @@ def convert(item):
     while i < len(item):
         quantity = int(item[i])
         coin = item[i+1]
-
+#Continues to loop until there are no more items in the list to take values from.
         if coin in ["penny", "pennies"]:
             dollar = 0.01
         elif coin in ["nickel", "nickels"]:
@@ -24,12 +24,13 @@ def convert(item):
             dollar = 0.10
         elif coin in ["quarter", "quarters"]:
             dollar = 0.25
-
+#Looks for keywords in the statement and changes the values of "quantity" and "dollar" to multiply and add on to the "final_total"
         final_total += quantity * dollar
         i += 2
-
+# i += 2 continues the loop by moving to the next pair in the list.
     print(f"You have: ${final_total:.2f}")
 
 clean_list = remove()
 convert(clean_list)
+
 
