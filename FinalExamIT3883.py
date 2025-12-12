2b619a16124dd10981d69077a35fd893099effb4
noboyboe <formaldavid7@gmail.com>
2025-12-11 19:24:20 -0500

    Update FinalExamIT3883.py

--- FinalExamIT3883.py
+++ FinalExamIT3883.py
@@ -1,5 +1,4 @@
 statement = input("State an amount of money: ")
-print("Enter 'STOP' if you wish to end the program.")
 
 def remove():
     item = statement.split()
@@ -10,32 +9,27 @@ def remove():
 #remove() splits the statement into a list and removes "and" to make it easier to convert.
 
 def convert(item):
-    conversion = []
+    final_total = 0
+    i = 0
 
-    for x in item[0:2]:
-        conversion.append(x)
-#Items are appended into the "conversion" list as pairs.
-    quantity = int(conversion[0])
-    conversion.remove(conversion[0])
-#The first number is the quantity and is removed from the "conversion" list for later values to be appended.
-    for x in conversion:
-        if x == "penny" or "pennies":
-            x = 0.01
-        elif x == "nickel" or "nickels":
-            x = 0.05
-        elif x == "dime" or "dimes":
-            x = 0.10
-        elif x == "quarter" or "quarters":
-            x = 0.25
-#This for loop identifies the keywords in a statement and converts it into values.
-        dollar = x
-        conversion.remove(conversion[0])
-#The second number is the dollar amount and is removed from the "conversion" list for later values to be appended.
-        total = quantity * dollar
+    while i < len(item):
+        quantity = int(item[i])
+        coin = item[i+1]
 
-        print(f"You have: ${total:.2f}")
+        if coin in ["penny", "pennies"]:
+            dollar = 0.01
+        elif coin in ["nickel", "nickels"]:
+            dollar = 0.05
+        elif coin in ["dime", "dimes"]:
+            dollar = 0.10
+        elif coin in ["quarter", "quarters"]:
+            dollar = 0.25
 
-    print(conversion)
+        final_total += quantity * dollar
+        i += 2
+
+    print(f"You have: ${final_total:.2f}")
 
 clean_list = remove()
 convert(clean_list)
+
